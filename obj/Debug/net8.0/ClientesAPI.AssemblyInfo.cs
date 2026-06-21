69911fb7aed26c78063d1cbf02c36270f9c18bf5
gordo240620 <manuelpacheco031105@gmail.com>
2026-06-20 18:06:01 -0600

    Version 2: Creacion del archivo Dockerfile

--- obj/Debug/net8.0/ClientesAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/ClientesAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClientesAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+716ae80711928d580ff809d1e2191162378a709c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClientesAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClientesAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
